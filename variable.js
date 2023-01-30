e952215297cae48d3af58f674942faa62564e94b
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-01-31 02:48:53 +0600

    55

--- variable.js
+++ variable.js
@@ -4,9 +4,9 @@ var age = 22;
 var temperature = 38;
 var weight = 50;
 var money = 200;
-var education = 8;
+var education = 8; // all are numeric data type 
 
-var name = "sodor uddin";
+var name = "sodor uddin";   // all are string data type 
 var location = "ami ekhon bandorbon";
-var special = "borsha";
+var special = 'borsha';
 
