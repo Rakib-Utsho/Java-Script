a602bb1eb4625eb0d3846fc1c62f0e896a3fc1f5
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-01-31 02:25:11 +0600

    1

---
+++ math.js
@@ -0,0 +1,18 @@
+/***
+ * 1. number ==> 55, 898,-32
+ * 2. Integer: 2, 5
+ * 3. float: decimal: 2.5,13.67,32.69
+ * 4. operation: addition, subtraction, multiplication, division, remainder(mod)
+ * 5. Percentage
+ * 6. Average
+ * 7. comparison of number
+ * 8. oikik method
+ * 9. maximum, minimum, avg, sum, 
+ * 10. series related math
+ * 11. even odd
+ * EXTRA:
+ * 1. Prime Number
+ * 2. Permutation
+ * ::::ALWAYS::::
+ * Jodi lage sikhte hobe
+ */
