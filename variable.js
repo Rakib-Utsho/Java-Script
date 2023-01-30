f818e05ad061c165ad8dfdf46e6223a260613a27
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-01-31 02:46:39 +0600

    1

---
+++ variable.js
@@ -0,0 +1,12 @@
+// vary + able = variable
+var price = 13;
+var age = 22;
+var temperature = 38;
+var weight = 50;
+var money = 200;
+var education = 8;
+
+var name = "sodor uddin";
+var location = "ami ekhon bandorbon";
+var special = "borsha";
+
