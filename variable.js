1ed0c6b60b5015bc8af704a21113d8452c524d35
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-01-31 02:53:05 +0600

    7

--- variable.js
+++ variable.js
@@ -1,4 +1,5 @@
 // vary + able = variable
+// numeric
 var price = 13;
 var age = 22;
 var temperature = 38;
@@ -6,7 +7,14 @@ var weight = 50;
 var money = 200;
 var education = 8; // all are numeric data type 
 
+//string
 var name = "sodor uddin";   // all are string data type 
 var location = "ami ekhon bandorbon";
 var special = 'borsha';
+
+// Boolen
+var series = true;
+var isFullMarks= true;
+var isSingle= false;
+
 
