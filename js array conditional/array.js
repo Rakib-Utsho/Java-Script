d12067c5c9c33b5b0c5ad9b77a1916edc22029b4
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-01 21:29:27 +0600

    array

---
+++ js array conditional/array.js
@@ -0,0 +1,8 @@
+var friendsAge=[11,21,45,17,14,105,6];
+
+var picnicFee=[5000,2000,4000,150];
+var nayikes = ['mahi', 'opu', 'sabnur', 'sabana'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(picnicFee);
+console.log(picnicFee.length);
