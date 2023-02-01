b8125dbf7eb20197b44d7941ec902e4063c338b0
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-01 19:48:19 +0600

    1

--- operation.js
+++ operation.js
@@ -1,9 +1,27 @@
 var onionPrice = 20;
 var eggPrice = 10;
 
+// addition, substraction
 var totoalPrice = onionPrice + eggPrice;
 // console.log(onionPrice);
 // console.log(eggPrice);
 // console.log(onionPrice+eggPrice);
 
-console.log(totoalPrice);
+// console.log(totoalPrice);
+
+var priceDiffernce = onionPrice -eggPrice
+// console.log(priceDiffernce);
+
+// multipication
+var orangePrice = 20;
+var quantity = 7;
+
+var totlaCost = orangePrice *quantity
+// console.log(totlaCost);
+
+// Division
+var money = 500;
+var palyer =10;
+
+var eachPlayer = money / palyer;
+console.log(eachPlayer);
