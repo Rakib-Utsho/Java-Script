f1b5a65846a8220212020098adffaf869e842383
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-01 19:07:56 +0600

    first

---
+++ variableName.js
@@ -0,0 +1,9 @@
+var price =29;
+
+// 1. variable name can't be any keywords
+
+var false = 96;
+var return = true;
+
+// 2. variable name has to be in one work.No space
+var my_home-adress= "New California"
