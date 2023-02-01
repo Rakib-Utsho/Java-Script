b2e07e99542ff6f7a24dbdef5887edb2f1fd4ecf
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-01 19:54:26 +0600

    1

---
+++ shorthand.js
@@ -0,0 +1,16 @@
+// variable declaration
+var tomatoPrice = 25;
+//variable value update
+tomatoPrice = 30;
+
+//add some thing to the previous value
+
+var Price1 = 31;
+var price2 = 10;
+
+// set a new value
+// Price1= 35;
+
+// add some value to the existing value
+Price1 = Price1 + 10;
+console.log(Price1);
