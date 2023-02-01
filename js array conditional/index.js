d0f0b78e1a17461411270a900c66ae8a8d117777
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-02 02:13:10 +0600

    array-100

--- js array conditional/index.js
+++ js array conditional/index.js
@@ -1,10 +1,14 @@
 var numbers = [45, 68, 78, 56,  89, 98];
 
-// get element by index
+//1. get element by index
 var element = numbers[1]
 console.log(element)
-
+//2. set element value by index
 numbers[1] = 77;
+numbers[3] = 44;
 
 console.log(numbers[1]);
-console.log(numbers);
+console.log(numbers[3]);
+// 3. find index of an element
+var positionIndex = numbers.indexOf(89)
+console.log(positionIndex);
