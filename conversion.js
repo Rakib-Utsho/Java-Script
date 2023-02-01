44135292e93601783e1abcbc1f8d459abcc531c4
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-01 20:15:37 +0600

    1

--- conversion.js
+++ conversion.js
@@ -8,3 +8,12 @@ var fistName = 'Jennie';
 var lastName = 'Kim';
 var fullName = fistName + ' ' + lastName;
 console.log(fullName);
+
+// convert string to number
+
+var p1 = '31';
+var p2 = 10;
+var total = p1 + p2;
+// console.log(p1);
+var pp1 = parseInt(p1);
+console.log(pp1);
