d9e3f53b8d9f737447f9e3b9ef53167102813f88
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-02 15:32:37 +0600

    array-100

---
+++ js array conditional/push-pop.js
@@ -0,0 +1,15 @@
+//use push to add element on array
+var numbers = [78, 45, 98, 45];
+numbers.push(63)
+console.log(numbers);
+
+var friends = ['Balam', 'Kalam', 'Salam'];
+friends.push('Gelam');
+friends.push('pailam')
+friends.push('Sabbir','Mahin')
+console.log(friends);
+
+//use pop to get last element
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
