67342b81dae2e8f6452f8e53de0b89c0d668b2d1
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-02 16:19:39 +0600

    multiple-condition

--- js array conditional/multiple-conditions.js
+++ js array conditional/multiple-conditions.js
@@ -1,15 +1,35 @@
-var isGraduated = true;
-var salary = 25000
-
+// var isGraduated = true;
+// var salary = 75000;
+// var cars = 1;
 // if(isGraduated == true){
 //     console.log('eso biye kore feli');
 // }
 // else{
 //     console.log('tor kopale biya nai');
 // }
-if(isGraduated == true && salary > 50000){
-    console.log('eso biye kore feli');
+// if(isGraduated == true && salary > 50000){
+//     console.log('eso biye kore feli');
+// }
+// else{
+//     console.log('tor kopale biya nai');
+// }
+// if(isGraduated == true && salary > 50000 && cars >= 1){
+//     console.log('eso biye kore feli');
+// }
+// else{
+//     console.log('tor kopale biya nai');
+// }
+var isGraduated = false;
+var salary = 31000;
+var cars = 0;
+
+if(isGraduated === true || salary > 50000 || cars >= 1){
+    console.log('eso biye prem kori');
 }
 else{
-    console.log('tor kopale biya nai');
+    console.log('tor friend er number de');
 }
+
+
+
+
