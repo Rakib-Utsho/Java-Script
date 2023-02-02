16d5ccf0bf41c6752e3f96e54500ea0feb584316
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-02 15:47:17 +0600

    array-comperison

--- js array conditional/comparison.js
+++ js array conditional/comparison.js
@@ -2,4 +2,17 @@
 // console.log(5 > 6);
 // console.log(5==6);
 // console.log(6==6);
-console.log(5!=6);
+// console.log(5!=6);
+// console.log(15!=15);
+// less then or equal
+// console.log(5 <=6 );
+// console.log(16 <=6 );
+
+//greater then or equal
+// console.log( 5 >= 6);
+console.log( 5 >= 2);
+
+// multiple condition
+
+// money1 > money2 && result1 > result2 && height1>height2
+// money1 > money2 || result1 > result2
