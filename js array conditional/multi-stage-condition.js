2af9802c115be164794c979f7357b26b31e6f1df
Rakib-Utsho <rakibutsho1920@gmail.com>
2023-02-02 21:38:13 +0600

    basic-done

---
+++ js array conditional/multi-stage-condition.js
@@ -0,0 +1,17 @@
+var money = 30;
+var danishPrice = 45;
+var butterbonPrice = 35;
+var toastBiscuit = 20;
+
+if(danishPrice < money){
+    console.log("Dan Dan Dan danish Khamu");
+}
+else if(butterbonPrice < money){
+    console.log('Butter bon Khamu');
+}
+else if(toastBiscuit < money){
+    console.log("Cha diya Tost Khamu");
+}
+else{
+    console.log("Ami Fokonni");
+}
---
+++ js array conditional/nested.js
@@ -0,0 +1,11 @@
+var math = true;
+var geometry = true;
+ 
+if(math == true){
+    if(geometry == true){
+
+    }
+    else{
+        consol
+    }
+}
